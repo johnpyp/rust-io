d7db54492eb6a0aec6e1a29424cdc6d13288d05a
johnpyp <johnpaulpenaloza@gmail.com>
2020-06-23 15:36:44 -0400

    FASTER

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
-use rayon::prelude::*;
+// use rayon::prelude::*;
+use crossbeam_channel::unbounded;
 use std::error::Error;
 use std::fs;
+use std::thread;
 use std::time::Instant;
 use uuid::Uuid;
 
+static NTHREADS: i32 = 8;
+static COUNT: i32 = 10_000_000;
 fn main() -> Result<(), Box<dyn Error>> {
+    if fs::metadata("uuids.txt").is_ok() {
+        fs::remove_file("uuids.txt")?;
+    }
+
     let start = Instant::now();
-    let res = (0..10_000_000)
-        .into_par_iter()
-        .map(|_i| Uuid::new_v4().to_string())
-        .collect::<Vec<String>>()
-        .join("\n");
+    let (tx, rx) = unbounded();
+
+    let per_thread = COUNT / NTHREADS;
+    println!("Threads: {}", NTHREADS);
+    println!("Per thread: {}", per_thread);
+    println!("Total: {}", per_thread * NTHREADS);
+
+    for t in 0..NTHREADS {
+        let thread_tx = tx.clone();
+        thread::spawn(move || {
+            for _i in 0..per_thread {
+                thread_tx.send(Uuid::new_v4().to_string()).unwrap()
+            }
+            println!("thread {} finished", t);
+        });
+    }
+
+    let mut res: String = "".to_string();
+    let mut count = 0;
+    for rec in rx.iter() {
+        res.push_str(&rec);
+        res.push_str("\n");
+        count += 1;
+        if count == COUNT {
+            break;
+        }
+    }
 
     fs::write("uuids.txt", res)?;
 
