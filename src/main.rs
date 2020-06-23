df5ca0b45ca3a03ddbe4811201b12a15ebb83107
johnpyp <johnpaulpenaloza@gmail.com>
2020-06-23 17:12:13 -0400

    Gen speed

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::time::Instant;
 use uuid::Uuid;
 
 fn main() {
-    if fs::metadata("uuids.txt").is_ok() {
-        fs::remove_file("uuids.txt").unwrap();
-    }
+    // if fs::metadata("uuids.txt").is_ok() {
+    //     fs::remove_file("uuids.txt").unwrap();
+    // }
     let start = Instant::now();
     let res = (0..10_000_000)
         .into_par_iter()
@@ -14,7 +14,8 @@ fn main() {
         .collect::<Vec<String>>()
         .join("\n");
 
-    fs::write("uuids.txt", res).unwrap();
+    // fs::write("uuids.txt", res).unwrap();
+    println!("{}", res.len());
 
     let duration = start.elapsed();
 
