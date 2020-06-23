fbf327b4dba857035034bcde3e2a09ab6f110da3
johnpyp <johnpaulpenaloza@gmail.com>
2020-06-23 14:45:09 -0400

    Added timer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rayon::prelude::*;
 use std::error::Error;
 use std::fs;
+use std::time::Instant;
 use uuid::Uuid;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
     let res = (0..10_000_000)
         .into_par_iter()
         .map(|_i| Uuid::new_v4().to_string())
@@ -11,5 +13,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         .join("\n");
 
     fs::write("uuids.txt", res)?;
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed: {:?}", duration);
     Ok(())
 }
